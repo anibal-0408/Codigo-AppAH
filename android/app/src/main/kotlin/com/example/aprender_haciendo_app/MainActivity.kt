72a9fc5821e8b2829729c138a67c266b0e35a7e5
anibal-0408 <anibaldani04@gmail.com>
2021-12-17 12:19:04 -0600

    CodigpAppAH
    Este es el codigo del app app de AH

---
+++ android/app/src/main/kotlin/com/example/aprender_haciendo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aprenderhaciendoapp.aprender_haciendo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ build/app/generated/source/buildConfig/debug/com/aprenderhaciendoapp/aprender_haciendo_app/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aprenderhaciendoapp.aprender_haciendo_app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.aprenderhaciendoapp.aprender_haciendo_app";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 11;
+  public static final String VERSION_NAME = "1.0.0";
+}
